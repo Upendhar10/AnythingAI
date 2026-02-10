d7eca4ccbca757757c9aca7cf52d0e683f0815b7
Upendhar <upendharnm.dev@gmail.com>
2026-02-10 19:02:02 +0530

    backend ( chore ) : Initialize a simple server with express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+import dotenv from 'dotenv';
+dotenv.config();
+
+import {PORT} from "./config/env.config";
+import app from './app';
+
+app.listen(PORT, () => {
+    console.log('Server is running at PORT :', PORT);
+})
