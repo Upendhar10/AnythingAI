3d8fd8104cfe0cdd1dd849b8a10a1fbaf053669b
Upendhar <upendharnm.dev@gmail.com>
2026-02-11 16:04:35 +0530

    backend ( feat ) : design API's to get and update user profile

---
+++ backend/src/controllers/user.controllers.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+
+import { User } from "../models/user.models";
+
+const getCurrentUser = async (req : Request, res: Response) => {
+    try {
+
+        const userId = req.user?.userId;
+
+        const user = await User.findById(userId).select('-password');
+
+        if(!user){
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+
+        return res.status(200).json({ user});
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to fetch user profile",
+        });
+    }
+}
+
+const updateUserProfile = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user?.userId;
+    const { fullName } = req.body;
+
+    if (!fullName || fullName.trim().length < 3) {
+      return res.status(400).json({
+        message: "Full name must be at least 3 characters",
+      });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { fullName },
+      { new: true }
+    ).select("-password");
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Profile updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to update profile",
+    });
+  }
+};
+
+export {
+    getCurrentUser,
+    updateUserProfile
+}
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 
 import { registerUser, loginUser } from "../controllers/auth.controllers";
+import authMiddleware from "../middleware/auth.middleware";
+import { getCurrentUser, updateUserProfile } from "../controllers/user.controllers";
 
 const router = Router();
 
+// public routes
 router.post('/register', registerUser);
 router.post('/login', loginUser)
+
+// private routes
+router.get('/me', authMiddleware, getCurrentUser);
+router.patch('/profile', authMiddleware, updateUserProfile)
 
 export default router;
