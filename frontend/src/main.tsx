f546ae1966aeea512a10fa04ffa3d86c5992e5a2
Upendhar <upendharnm.dev@gmail.com>
2026-02-11 21:36:14 +0530

    frontend ( refactor ) : rename reduxTK with app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import './index.css'
 import App from './App.tsx'
-import store from './reduxTK/store.ts';
+import store from './app/store.ts';
 
 
 createRoot(document.getElementById('root')!).render(
