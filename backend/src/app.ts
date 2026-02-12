e2ca5781d36af85166b74f4329248c92b1b10972
Upendhar <upendharnm.dev@gmail.com>
2026-02-12 23:33:46 +0530

    backend ( feat ) : configure cors to test auth functionality

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,20 @@
+import cors from "cors"
 import express from 'express';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
 // Routes
 import authRouter from "./routes/auth.routes"
 
-app.use('api/v1/auth', authRouter);
+app.use('/api/v1/auth', authRouter);
 
 export default app;
