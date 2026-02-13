0c5ee7a1308513259d0ce7f4d2bb36fc7b6a1990
Upendhar <upendharnm.dev@gmail.com>
2026-02-13 23:01:41 +0530

    backend (fix) : update cors with live frontend url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,20 @@ import express from 'express';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  }),
-);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://anything-ai-seven.vercel.app/"
+]
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json());
 
